80e5095077c26bd0ab789801a699215cd71e11e9
GitauSam <njorogegitausamuel@gmail.com>
2020-12-09 17:30:47 +0300

    Start implementation of allConstruct solution

--- src/test/kotlin/com/example/demo/dynamicprogramming/AllConstruct.kt
+++ src/test/kotlin/com/example/demo/dynamicprogramming/AllConstruct.kt
@@ -12,4 +12,22 @@ class AllConstruct {
      * Elements may be reused.
      *
      */
+
+    fun allConstruct(target: String,
+                     wordBank: Array<String>): ArrayList<ArrayList<String>>{
+        if (target == "") return arrayListOf(arrayListOf<String>())
+
+        for (word in wordBank) {
+            if (target.indexOf(word) == 0) {
+                val suffix = target.substringAfter(word)
+                val suffixWays = allConstruct(suffix, wordBank)
+                val targetWays = arrayListOf<ArrayList<String>>()
+                val count = 0
+                suffixWays.forEach {
+                    targetWays[count] = it
+                    targetWays[count][0] = word
+                }
+            }
+        }
+    }
 }
